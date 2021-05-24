5509b732226a45890e71687cc8a97d0a03fa83ce
abvg9 <deveavg@gmail.com>
2021-05-24 23:25:37 +0200

    Update license.

--- src/driver/led/led.h
+++ src/driver/led/led.h
@@ -1,3 +1,21 @@
+/**
+ * This file is part of the abvg9 distribution (https://github.com/abvg9/Distributed_localization_DWM1001).
+ * Copyright (c) 2021 Álvaro Velsco García, Madrid, Spain.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef _LED_
 #define _LED_
 
